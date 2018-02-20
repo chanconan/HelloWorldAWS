d8f893730adfde020f8e8cabd1dc7fb55d03db58
Conan Chan <chanconan67@gmail.com>
2018-02-20 11:00:12 -0500

    first commit

---
+++ src/main/webapp/scripts/main.js
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+
+function sendAjaxGet(url, func){
+	var xhr = new XMLHttpRequest() || new ActiveXObject("Microsoft.HttpRequest");
+	xhr.onreadystatechange = function(){
+		if(this.readyState == 4 && this.status == 200){
+			func(this);
+		}
+	}
+	xhr.open("GET", url, true);
+	xhr.send();
+}
+
+function showMeTheWorld(xhr){
+	if(xhr.responseText){
+		document.getElementById("showMeTheWorld").innerHTML = xhr.responseText;
+	}
+}
+
+window.onload = function(){
+	document.getElementById("takeMeThere").addEventListener('click', function(){
+		sendAjaxGet("/helloWorldAws/takeMeThere", showMeTheWorld);
+	}, false);
+	
+}
